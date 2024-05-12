393b8b664e7f6c105100e77a1f3c9e857a4c53e9
1ambuj <110759439+1ambuj@users.noreply.github.com>
2024-05-12 22:11:06 +0530

    Setup my protfolio app and create home page

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Home";
+import {BrowserRouter} from "react-router-dom"
+
+function App() {
+
+
+  return (
+    <BrowserRouter>
+       <Header />
+    </BrowserRouter>
+    
+  )
+}
+
+export default App
---
+++ src/components/Home.jsx
@@ -0,0 +1,34 @@
+import React from "react"
+import "../App.css"
+export default function Home(){
+    return (
+        <header className="header" role="banner" id="top">
+                <div className="row">
+                <nav className="nav" role="navigation">
+                    <ul className="nav__items">
+                    <li className="nav__item"><a href="#work" className="nav__link">Work</a></li>
+             
+                    <li className="nav__item">
+                        <a href="#about" className="nav__link">About</a>
+                    </li>
+                    <li className="nav__item">
+                        <a href="#contact" className="nav__link">Contact</a>
+                    </li>
+                    </ul>
+                </nav>
+                </div> 
+                <div className="header__text-box row">
+                <div className="header__text">
+                    <h1 className="heading-primary">
+                    {/* <!-- Replace the following name with your name --> */}
+                    <span>Ambuj Mishra</span>
+                    </h1>
+                    
+                    <p>A Front-End Developer based in London, UK
+            </p>
+                    <a href="#contact" className="btn btn--pink">Get in touch</a>
+                </div>
+                </div>
+        </header>  
+    )
+}
